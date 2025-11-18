1d22f33cf780de51ca0d0344d59da701dfe5ba6c
taerimiiii <shipton0201@gmail.com>
2025-11-19 00:43:24 +0900

    feat: 상/하단 바 구현

---
+++ app/src/main/java/com/example/oop/ui/calender/CalendarComponents.kt
@@ -0,0 +1,2 @@
+package com.example.oop.ui.calender
+
---
+++ app/src/main/java/com/example/oop/ui/calender/CalendarUiState.kt
@@ -0,0 +1,9 @@
+package com.example.oop.ui.calender
+
+import java.time.LocalDate
+
+// 화면 표현에 필요한 상태 묶음
+data class CalendarUiState(
+    // 임시 코드
+    val selectedDate: LocalDate = LocalDate.now()
+)
