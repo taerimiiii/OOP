784386e46b3548f5a2b4b04c75d12feb8d8dc1ad
taerimiiii <shipton0201@gmail.com>
2025-11-19 17:57:47 +0900

    test: 임시 커밋

--- app/src/main/java/com/example/oop/ui/calender/CalendarScreen.kt
+++ app/src/main/java/com/example/oop/ui/calender/CalendarScreen.kt
@@ -7,15 +7,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.kizitonwose.calendar.core.daysOfWeek
 
 // UI 화면을 구성하는 Composable 함수
 @Composable
 fun CalendarScreen(modifier: Modifier = Modifier) {
+    val daysOfWeek = daysOfWeek()
+
     Column(
         modifier = modifier.fillMaxSize().padding(horizontal = 5.dp, vertical = 15.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         CalendarTitleCard(text = "약먹자 님의 섭취 기록", height = 40.dp)
+        MonthCalendar()
     }
 
 
