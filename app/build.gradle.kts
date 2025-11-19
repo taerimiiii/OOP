a143996e145fd0d67b7b48c9162c90f8de4c0ee2
taerimiiii <shipton0201@gmail.com>
2025-11-19 17:57:47 +0900

    test: 임시 커밋

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.monitor)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/oop/ui/calender/CalendarUtils.kt
+++ app/src/main/java/com/example/oop/ui/calender/CalendarUtils.kt
@@ -1,2 +1,1 @@
 package com.example.oop.ui.calender
-
