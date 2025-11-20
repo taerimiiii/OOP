3ce12f4ff521c8baa3217fd90c56462afdd830c9
taerimiiii <shipton0201@gmail.com>
2025-11-20 10:28:57 +0900

    refactor: 상/하단바 로직만 남기기

--- app/src/main/java/com/example/oop/ui/calender/CalendarScreen.kt
+++ app/src/main/java/com/example/oop/ui/calender/CalendarScreen.kt
@@ -1,24 +1,27 @@
 package com.example.oop.ui.calender
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.kizitonwose.calendar.core.daysOfWeek
+import com.example.oop.ui.PillTopBar
 
 // UI 화면을 구성하는 Composable 함수
 @Composable
 fun CalendarScreen(modifier: Modifier = Modifier) {
-    val daysOfWeek = daysOfWeek()
-
-    Column(
-        modifier = modifier.fillMaxSize().padding(horizontal = 5.dp, vertical = 15.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    // 실제 캘린더 UI
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        CalendarTitleCard(text = "약먹자 님의 섭취 기록", height = 40.dp)
-        MonthCalendar()
+        Text(
+            text = "캘린더 화면을 구성하세요.",
+            modifier = Modifier.padding(24.dp)
+        )
     }
 }
