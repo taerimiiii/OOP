1e8e052ecb1dc8e017cf459b6782e00df75633bc
flashbomb625 <a01080065205@gmail.com>
2025-11-21 16:59:01 +0900

    검색창 수정 완료

--- app/src/main/java/com/example/oop/ui/view/SearchResultScreen.kt
+++ app/src/main/java/com/example/oop/ui/view/SearchResultScreen.kt
@@ -111,8 +111,8 @@ fun SearchResultScreen(
                 .fillMaxSize()
                 .background(Color.White)
                 .padding(contentPadding)
-                .padding(horizontal = 16.dp, vertical = 12.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+                .padding(horizontal = 16.dp, vertical = 8.dp), //vertical 12 -> 8
+            verticalArrangement = Arrangement.spacedBy(12.dp) //16 -> 12
         ) {
             items(medicines) { medicine ->
                 MedicineResultCard(
@@ -143,7 +143,7 @@ fun SearchTopBar(
         modifier = Modifier
             .fillMaxWidth()
             .background(Color.White)
-            .padding(16.dp)
+            .padding(top = 50.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
     ) {
         // 로고
         Image(
