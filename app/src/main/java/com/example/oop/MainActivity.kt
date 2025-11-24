865604eb1ee7d551f15ead228dc30f561ef1a499
ryudonghee <rdh071474@gmail.com>
2025-11-24 09:56:24 +0900

    develop받음

--- app/src/main/java/com/example/oop/MainActivity.kt
+++ app/src/main/java/com/example/oop/MainActivity.kt
@@ -6,22 +6,47 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.oop.ui.BottomNavBar
+import com.example.oop.ui.PillTopBar
+import com.example.oop.ui.Search.SearchScreen
+import com.example.oop.ui.calendar.CalendarScreen
+import com.example.oop.ui.home.HomeScreen
 import com.example.oop.ui.theme.OOPTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             OOPTheme(darkTheme = false) {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                val selectedBottomItem = remember { mutableStateOf(1) }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    // 상단 바 호출
+                    topBar = {
+                        PillTopBar(
+                            useGreenBackground = true
+                        )
+                    },
+                    // 하단 바 호출
+                    bottomBar = {
+                        BottomNavBar(
+                            selectedItem = selectedBottomItem.value,
+                            onItemClick = { selectedBottomItem.value = it }
+                        )
+                    }
+                ) { innerPadding ->
+                    Content(
+                        selectedItem = selectedBottomItem.value,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +55,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// 화면 관리 컴포저블
+// 메인에 넣어도 되나...??
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Content(
+    selectedItem: Int,
+    modifier: Modifier = Modifier
+) {
+    when (selectedItem) {
+        0 -> SearchScreen(modifier = modifier)
+        1 -> HomeScreen(modifier = modifier)
+        2 -> CalendarScreen(modifier = modifier)
+        else -> HomeScreen(modifier = modifier) // 이거 맘에 안 듦.
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreenPreview() {
     OOPTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
