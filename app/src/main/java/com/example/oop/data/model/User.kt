be01ce068c004aa041f8dca320347361c99902c7
taerimiiii <shipton0201@gmail.com>
2025-11-26 22:44:04 +0900

    fix: 임시 데이터 수정

--- app/src/main/java/com/example/oop/data/model/User.kt
+++ app/src/main/java/com/example/oop/data/model/User.kt
@@ -4,7 +4,4 @@ data class User(
     val uid: String = "",
     val email: String = "",
     val userName: String? = null,
-//    val uid: String = "1",
-//    val email: String = "test@example.com",
-//    val userName: String? = "테스터1"
 )
