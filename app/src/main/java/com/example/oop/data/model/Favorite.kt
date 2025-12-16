611d0addd23efe010e697790a07d9bccacedbb4d
flashbomb625 <a01080065205@gmail.com>
2025-12-16 18:08:30 +0900

    feat: 즐겨찾기 성공!!

--- app/src/main/java/com/example/oop/data/model/Favorite.kt
+++ app/src/main/java/com/example/oop/data/model/Favorite.kt
@@ -1,5 +1,20 @@
 package com.example.oop.data.model
 
+import androidx.compose.runtime.mutableStateListOf
 data class Favorite(
     val itemSeq: String = "",
 )
+
+
+object FavoriteStorage {
+    val favoriteList = mutableStateListOf<String>()
+
+    // 즐겨찾기 추가/삭제 함수
+    fun toggleFavorite(itemSeq: String) {
+        if (favoriteList.contains(itemSeq)) {
+            favoriteList.remove(itemSeq) // 이미 있으면 삭제
+        } else {
+            favoriteList.add(itemSeq)    // 없으면 추가
+        }
+    }
+}
